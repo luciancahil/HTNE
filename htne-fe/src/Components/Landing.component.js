25d163f35d8eacba3aa9d9aa7b3d7a1d606058ca
Roy He <royhe62@yahoo.ca>
2021-01-15 20:59:06 -0800

    added functionality to button and form on landing page

--- htne-fe/src/Components/Landing.component.js
+++ htne-fe/src/Components/Landing.component.js
@@ -3,15 +3,37 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import {Form, Button} from 'react-bootstrap';
 
 class Landing extends React.Component {
+    constructor(props){
+        super(props);
+        this.onSubmit = this.onSubmit.bind(this);
+        this.onURLChange = this.onURLChange.bind(this);
+        
+        this.state = {
+            url: ""
+        }
+    }
+
+    onURLChange(e){
+        e.preventDefault();
+        this.setState({
+            url: e.target.value
+        })
+        
+    }
+
+    onSubmit(e){
+        e.preventDefault();
+        alert(this.state.url);
+    }
   render() {
     return(
         <div id = "Landing Page">
             <h1 className = "App-header">Is It Fake News?</h1>
             <Form>
-                    <Form.Control id = "url-entry" size="lg" type="text" placeholder="https://zapatopi.net/treeoctopus/sightings.html" />
+                    <Form.Control id = "url-entry" size="lg" type="text" placeholder="https://zapatopi.net/treeoctopus/sightings.html" onChange = {this.onURLChange} />
             </Form>
             <div id = "button-outer">
-                <Button id = "submit-button" variant="primary" type="submit">
+                <Button id = "submit-button" variant="primary" type="submit" onClick = {this.onSubmit}>
                     Check!
                 </Button>
             </div>
